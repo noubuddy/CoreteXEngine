b24c08deea8324af63e67456f2dd2826bcebfeba
noubuddy <anton.nich00@gmail.com>
2023-04-09 22:03:23 +0300

    modified vertices and indices arrays

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+#include <vector>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb/stb_image.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
 
 #include "Shader.h"
 #include "VAO.h"
@@ -14,56 +14,91 @@
 #include "Texture.h"
 #include "Camera.h"
 
+GLfloat vertices[] =
+{
+    // Front Face
+    -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Bottom-left
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom-right
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top-right
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // Top-left
+
+    // Back Face
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom-right
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Bottom-left
+    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // Top-left
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top-right
+
+    // Top Face
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom-left
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Bottom-right
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // Top-right
+    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top-left
+
+    // Bottom Face
+    -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, // Bottom-right
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Bottom-left
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, // Top-left
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, // Top-right
+
+    // Right face
+    0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+    0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+    0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f,
+
+    // Left face
+    -0.5f, -0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    -0.5f, -0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
+    -0.5f, 0.5f, 0.5f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+    -0.5f, 0.5f, -0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f
+};
+
+GLuint indices[] =
+{
+    // Front Face
+    0, 1, 2,
+    0, 3, 2,
+    // Back Face
+    4, 5, 6,
+    4, 7, 6,
+    // Top face
+    8, 9, 10,
+    8, 11, 10,
+    // Bottom Face
+    12, 13, 14,
+    12, 15, 14,
+    // Right Face
+    16, 17, 18,
+    16, 19, 18,
+    // Left Face
+    20, 21, 22,
+    20, 23, 22,
+};
+
+std::vector<glm::vec3> cubePositions
+    = {
+        glm::vec3(1.0f, 1.0f, 0.0f),
+        glm::vec3(2.0f, 1.0f, 0.0f),
+        glm::vec3(3.0f, 1.0f, 0.0f),
+        glm::vec3(4.0f, 1.0f, 0.0f),
+        glm::vec3(5.0f, 1.0f, 0.0f),
+        glm::vec3(1.0f, 1.0f, 1.0f),
+        glm::vec3(2.0f, 1.0f, 1.0f),
+        glm::vec3(3.0f, 1.0f, 1.0f),
+        glm::vec3(4.0f, 1.0f, 1.0f),
+        glm::vec3(5.0f, 1.0f, 1.0f)
+    };
+
+
 int main()
 {
-    int width = 1000;
-    int height = 1000;
+    const int width = 1000;
+    const int height = 1000;
+    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLfloat vertices[] =
-    {
-        -0.5f,  0.0f,   0.5f,   1.0f, 0.0f, 0.0f,    0.0f, 0.0f, // A1
-        -0.5f,  0.0f,  -0.5f,   1.0f, 0.0f, 0.0f,    1.0f, 0.0f, // B1
-         0.5f,  0.0f,  -0.5f,   1.0f, 0.0f, 0.0f,    0.0f, 0.0f, // C1
-         0.5f,  0.0f,   0.5f,   1.0f, 0.0f, 0.0f,    1.0f, 0.0f, // D1
-        
-        -0.5f,  1.0f,   0.5f,   1.0f, 0.0f, 0.0f,    0.0f, 1.0f, // A2
-        -0.5f,  1.0f,  -0.5f,   1.0f, 0.0f, 0.0f,    1.0f, 1.0f, // B2
-         0.5f,  1.0f,  -0.5f,   1.0f, 0.0f, 0.0f,    0.0f, 1.0f, // C2
-         0.5f,  1.0f,   0.5f,   1.0f, 0.0f, 0.0f,    1.0f, 1.0f  // D2
-    };
-
-    GLuint indices[] =
-    {
-        0, 1, 2, 
-        0, 3, 2,
-        6, 5, 2, 
-        5, 1, 2, 
-        3, 7, 2, 
-        6, 7, 2, 
-        3, 0, 4, 
-        3, 7, 4,
-        0, 1, 5,
-        0, 4, 5,
-        4, 5, 6,
-        4, 7, 6
-    };
-
-    glm::vec3 cubePositions[] = {
-        glm::vec3( 1.0f,  1.0f,  0.0f),
-        glm::vec3( 2.0f,  1.0f,  0.0f),
-        glm::vec3( 3.0f,  1.0f,  0.0f),
-        glm::vec3( 4.0f,  1.0f,  0.0f),
-        glm::vec3( 5.0f,  1.0f,  0.0f),
-        glm::vec3( 1.0f,  1.0f,  1.0f),
-        glm::vec3( 2.0f,  1.0f,  1.0f),
-        glm::vec3( 3.0f,  1.0f,  1.0f),
-        glm::vec3( 4.0f,  1.0f,  1.0f),
-        glm::vec3( 5.0f,  1.0f,  1.0f)
-    };
 
     GLFWwindow* window = glfwCreateWindow(width, height, "Window", nullptr, nullptr);
     if (window == nullptr)
@@ -87,13 +122,16 @@ int main()
     vao1.LinkAttrib(vbo1, 0, 3, GL_FLOAT, 8 * sizeof(float), nullptr);
     vao1.LinkAttrib(vbo1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     vao1.LinkAttrib(vbo1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    
+
     vao1.Unbind();
     vbo1.Unbind();
     ebo1.Unbind();
 
     Texture texture("block.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
     texture.TexUnit(shader_program, "tex0", 0);
+
+    Texture texture2("block-top.jpg", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+    texture2.TexUnit(shader_program, "tex0", 0);
 
     glEnable(GL_DEPTH_TEST);
 
@@ -107,36 +145,33 @@ int main()
     while (!glfwWindowShouldClose(window))
     {
         // FPS couonter
-        double currentTime = glfwGetTime();
+        const double currentTime = glfwGetTime();
         nbFrames++;
-        if ( currentTime - lastTime >= 1.0 ){
-            std::cout << 1000.0/double(nbFrames) << std::endl;
+        if (currentTime - lastTime >= 1.0)
+        {
+            std::cout << 1000.0 / double(nbFrames) << std::endl;
             nbFrames = 0;
             lastTime += 1.0;
         }
-        
+
         glClearColor(0.29f, 0.66f, 0.87f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        
+
         camera.speed = 0.001f;
         camera.Inputs(window);
 
         texture.Bind();
         vao1.Bind();
 
-        for (int i = 0; i < 10; i++)
+        for (auto& cubePosition : cubePositions)
         {
             glm::mat4 model = glm::mat4(1.0f);
-            float angle = 20.0f * i;
-            
-            model = glm::translate(model, cubePositions[i]);
-            // model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-            
+            model = glm::translate(model, cubePosition);
+
             camera.Matrix(80.0f, 0.1f, 100.0f, shader_program, "camMatrix", model);
-            
-            glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, nullptr);
+            glDrawElements(GL_TRIANGLES, sizeof indices / sizeof(int), GL_UNSIGNED_INT, nullptr);
         }
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
