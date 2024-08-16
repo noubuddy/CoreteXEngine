d9855c8edcbec727afdb269935ad6dd74e15ff8c
noubuddy <anton.nich00@gmail.com>
2024-08-16 21:30:45 +0300

    modified LinkedList class

--- src/Core/Data/DataStructures/LinkedList.cpp
+++ src/Core/Data/DataStructures/LinkedList.cpp
@@ -1,1 +1,63 @@
-#include "LinkedList.h"
+// #include "LinkedList.h"
+//
+// template <typename T>
+// void LinkedList<T>::InsertNode(T* data)
+// {
+//     LinkedListNode* new_node = new LinkedListNode(data);
+//
+//     if (new_node == nullptr)
+//     {
+//         // TODO: add check
+//     }
+//
+//     if (head == nullptr)
+//     {
+//         head = new_node;
+//         return;
+//     }
+//
+//     LinkedListNode* node_iter = head;
+//     while (node_iter->next != nullptr)
+//     {
+//         node_iter = node_iter->next;
+//     }
+//
+//     node_iter->next = new_node;
+// }
+//
+// template <typename T>
+// void LinkedList<T>::DeleteNode(int NodeOffset)
+// {
+//     LinkedListNode* node_temp1 = head, *node_temp2 = nullptr;
+//     int list_length = 0;
+//
+//     if (head == nullptr)
+//     {
+//         // TODO: add check
+//         return;
+//     }
+//
+//     while (node_temp1 != nullptr)
+//     {
+//         node_temp1 = node_temp1->next;
+//         list_length++;
+//     }
+//
+//     if (list_length < NodeOffset)
+//     {
+//         // TODO: add check
+//         return;
+//     }
+//
+//     node_temp1 = head;
+//
+//     if (NodeOffset-- > 1)
+//     {
+//         node_temp2 = node_temp1;
+//         node_temp1 = node_temp1->next;
+//     }
+//
+//     node_temp2->next = node_temp1->next;
+//
+//     delete node_temp1;
+// }
--- src/Core/Data/DataStructures/LinkedList.h
+++ src/Core/Data/DataStructures/LinkedList.h
@@ -1,15 +1,41 @@
-#ifndef LINKED_LIST_CLASS_H
-#define LINKED_LIST_CLASS_H
-
-template <typename T>
-class LinkedList
-{
-public:
-    
-private:
-
-    
-    
-};
-
-#endif
+// #ifndef LINKED_LIST_CLASS_H
+// #define LINKED_LIST_CLASS_H
+//
+// #include <cstdlib>
+// #include <iostream>
+//
+// template <typename T>
+// class LinkedList
+// {
+//
+// private:
+//     typedef struct LinkedListNode
+//     {
+//         T* data;
+//         LinkedListNode* next;
+//
+//         LinkedListNode()
+//         {
+//             this->data = nullptr;
+//             this->next = nullptr;
+//         }
+//         
+//         LinkedListNode(T* data)
+//         {
+//             this->data = data;
+//             this->next = nullptr;
+//         }
+//     } LinkedListNode;
+//     
+//     LinkedListNode* head;
+//
+// public:
+//
+//     LinkedList() : head(nullptr) {}
+//
+//     void InsertNode(T* data);
+//
+//     void DeleteNode(int NodeOffset);
+// };
+//
+// #endif
