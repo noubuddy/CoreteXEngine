2bda3642ddb9d5e3eac15332bec9979a31d7a147
noubuddy <anton.nich00@gmail.com>
2025-03-29 15:24:40 +0200

    refactor Application | remove useless header

--- src/Core/Application.h
+++ src/Core/Application.h
@@ -3,55 +3,41 @@
 
 #include "Engine.h"
 #include "Interfaces/IApplication.h"
-#include <memory>
 
 template <typename App>
 class Application : public IApplication
 {
 public:
-    Application();
+    Application() : m_application(std::make_unique<App>()) {}
+    
     ~Application() override = default;
 
-    static App GetInstance();
+    static App GetInstance()
+    {
+        if (!m_application) {
+            m_application = std::make_unique<App>();
+            m_application->Initialize();
+        }
     
-    bool Initialize() override;
-    bool StartUp() override;
-    bool ShutDown() override;
+        return m_application;
+    }
+    
+    bool Initialize() override
+    {
+        return m_application->Initialize();
+    }
+    
+    bool StartUp() override
+    {
+        return m_application->StartUp();
+    }
+    
+    bool ShutDown() override
+    {
+        return m_application->ShutDown();
+    }
 
 private:
     std::unique_ptr<App> m_application;
 };
-
-template <typename App>
-Application<App>::Application() : m_application(std::make_unique<App>()) {}
-
-template <typename App>
-bool Application<App>::Initialize()
-{
-    return m_application->Initialize();
-}
-
-template <typename App>
-bool Application<App>::StartUp()
-{
-    return m_application->StartUp();
-}
-
-template <typename App>
-bool Application<App>::ShutDown()
-{
-    return m_application->ShutDown();
-}
-
-template <typename App>
-App Application<App>::GetInstance()
-{
-    if (!m_application) {
-        m_application = std::make_unique<App>();
-        m_application->Initialize();
-    }
-    
-    return m_application;
-}
-
 #endif
